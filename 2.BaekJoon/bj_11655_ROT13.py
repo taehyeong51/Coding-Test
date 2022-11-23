3edc5fb238711f29f20b4225775f5c3241931f78
taehyeong51 <107748183+taehyeong51@users.noreply.github.com>
2022-11-23 23:25:02 +0900

    Create bj_11655_ROT13.py

---
+++ 2.BaekJoon/bj_11655_ROT13.py
@@ -0,0 +1,18 @@
+S = list(input())
+
+alphabet = ['a','b','c','d','e','f','g','h','i',
+            'j','k','l','m','n','o','p','q','r','s',
+            't','u','v','w','x','y','z']
+capital_alphabet = ['A','B','C','D','E','F','G','H','I',
+                    'J','K','L','M','N','O','P','Q','R','S',
+                    'T','U','V','W','X','Y','Z']
+
+for i in range(len(S)):
+    if S[i]:
+        if S[i] in alphabet:
+            print(alphabet.index(S[i])+1)
+            S[i] = alphabet[(alphabet.index(S[i])+13)%len(alphabet)]
+        elif S[i] in capital_alphabet:
+            S[i] = capital_alphabet[(capital_alphabet.index(S[i])+13)%len(alphabet)]
+            
+print("".join(S))
